390d208e59d66c37d75620cc39a17b09ae6cea81
anhhieu1234 <70497519+anhhieu1234@users.noreply.github.com>
2020-11-09 15:12:47 +0700

    11/9/2020

--- src/app/customer/checkout/checkout.component.ts
+++ src/app/customer/checkout/checkout.component.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent extends BaseComponent implements OnInit {
-  items:any;
+  items2:any;
   total:any;
   public hoadonForm: FormGroup;
   constructor(injector: Injector,public  router: Router) { 
@@ -17,26 +17,29 @@ export class CheckoutComponent extends BaseComponent implements OnInit {
   }
 
   onSubmit(value: any) {
-    let hoadon = {ho_ten: value.ho_ten, dia_chi:value.dia_chi, sodt:value.sodt, email:value.email, listjson_chitiet:this.items};
-    this._api.post('/api/hoadon/create-hoa-don', hoadon).takeUntil(this.unsubscribe).subscribe(res => {
+    let hoadon = {ho_ten: value.ho_ten, dia_chi:value.dia_chi, sodt:Number.parseInt(value.sodt), email:value.email, listjson_chitiet:this.items2};
+    console.log(hoadon);
+    this._api.post('/api/hoadon/create-hoadon', hoadon).takeUntil(this.unsubscribe).subscribe(res => {
       alert('Tạo thành công');
       this.router.navigate(['']);
       this.total=0;
       this._cart.clearCart();
        }, err => { });      
- 
   }
 
   ngOnInit(): void {
     this.hoadonForm = new FormGroup({
       ho_ten: new FormControl('', Validators.required),
-      dia_chi: new FormControl('')       
+      dia_chi: new FormControl(''),
+      sodt:new FormControl(''),
+      email:new FormControl('',Validators.email)       
     });
+    this.items2=[];
 
     this._cart.items.subscribe((res) => {
-      this.items = res;
+      this.items2 = res;
       this.total = 0;
-      for(let x of this.items){ 
+      for(let x of this.items2){ 
         x.so_luong = +x.quantity;
         x.money = x.quantity * x.item_price;
         this.total += x.quantity * x.item_price;
